efcaeae89a316798fd37f5025e22dc7c0e693b45
Pamela Frieze <pfrieze@gmail.com>
2018-06-02 01:05:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,10 +61,9 @@ class Student
     WHERE name = ?
     SQL
 
-    DB[:conn].execute(sql, name)
-    #.map do |row|
-    #  self.new_from_db(row)
-    #end
+    DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+    end
   end
 
   def update
